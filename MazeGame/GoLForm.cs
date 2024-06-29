0bbcd8a306fd06f36625e24f2f4f8d67e7f8645d
Manuel König <79515423+Xilef12000@users.noreply.github.com>
2024-06-29 18:29:41 +0200

    get GoL State and Compare

--- MazeGame/GoLForm.cs
+++ MazeGame/GoLForm.cs
@@ -12,15 +12,23 @@ namespace MazeGame
 {
     public partial class GoLForm : Form
     {
+        private int[,] GoLState;
+        public int[,] GoLGoal;
+        private CheckBox[,] GoLCheckBox;
+        private int height;
+        private int width;
         public GoLForm(int[,] GoLGoal)
         {
+            this.GoLGoal = GoLGoal;
             InitializeComponent();
-            int height = GoLGoal.GetLength(0);
-            int width = GoLGoal.GetLength(1);
+            this.height = this.GoLGoal.GetLength(0);
+            this.width = this.GoLGoal.GetLength(1);
+            GoLState = new int[this.height, this.width];
+            GoLCheckBox = new CheckBox[this.height, this.width];
 
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < this.height; i++)
             {
-                for (int j = 0; j < width; j++)
+                for (int j = 0; j < this.width; j++)
                 {
                     CheckBox checkBox = new CheckBox();
                     checkBox.Appearance = Appearance.Button;
@@ -28,22 +36,60 @@ namespace MazeGame
                     checkBox.Name = $"checkBox{i}x{j}";
                     checkBox.Size = new Size(48, 48);
                     checkBox.TabIndex = 0;
-                    checkBox.Text = $"{(GoLGoal[i, j] == 1 ? "X" : "  ")}";
+                    checkBox.Text = $"{(this.GoLGoal[i, j] == 1 ? "X" : "  ")}";
                     checkBox.TextAlign = ContentAlignment.MiddleCenter;
                     checkBox.UseVisualStyleBackColor = true;
 
-                    if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    if (i == 0 || i == this.height - 1 || j == 0 || j == this.width - 1)
                     {
                         checkBox.Enabled = false;
                     }
                     this.Controls.Add(checkBox);
+                    GoLCheckBox[i,j] = checkBox;
                 }
             }
         }
-
+        private void getCheckboxStates()
+        {
+            for (int i = 0; i < this.height; i++)
+            {
+                for (int j = 0; j < this.width; j++)
+                {
+                    GoLState[i,j] = GoLCheckBox[i,j].Checked ? 1 : 0;
+                }
+            }
+        }
+        private bool compareStates(int[,] goal, int[,] state)
+        {
+            // a and b must be of [height, width]
+            // ckecks if all 1 in goal are 1 in state
+            for (int i = 0; i < this.height; i++)
+            {
+                for (int j = 0; j < this.width; j++)
+                {
+                    if (goal[i,j] == 1)
+                    {
+                        if (state[i,j] == 0)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
         private void startSimulation()
         {
-            this.DialogResult = DialogResult.OK;
+            int test = GoLGoal[0,0];
+            getCheckboxStates();
+            if (compareStates(GoLGoal, GoLState))
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                this.DialogResult = DialogResult.Cancel;
+            }
             this.Close();
         }
         private void Start_Click(object sender, EventArgs e)
