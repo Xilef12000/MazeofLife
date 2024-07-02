7107e3a385616418cbbbb1a51494f1bd04534349
alavanou <niklas.bachmann@posteo.de>
2024-07-02 17:01:58 +0200

    GOL link

--- MazeGame/GolItem.cs
+++ MazeGame/GolItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,26 @@ namespace MazeGame
         }
         public override void use(Player user)
         {
-           user.addItem(key);
+            int[,] GoLGoal = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0}
+            };
+            Form GoLForm = new GoLForm(GoLGoal);
+            GoLForm.ShowDialog();
+            DialogResult result = GoLForm.DialogResult;
+            if (result == DialogResult.OK)
+            {
+                Debug.WriteLine("OK");
+                user.addItem(key);
+                user.removeItem(this);
+            }
+            else
+            {
+                Debug.WriteLine("Not OK");
+            }
         }
 
     }
